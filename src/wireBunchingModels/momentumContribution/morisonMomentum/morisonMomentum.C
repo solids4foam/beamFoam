669b52c00c5a19c2e3ba665ea6129a0acb9d8990
Seevani Bali <b.seevani2013@gmail.com>
2025-11-17 10:49:29 +0000

    Updates to momentumContribution base class and derived morisonMomentum class

--- src/wireBunchingModels/momentumContribution/morisonMomentum/morisonMomentum.C
+++ src/wireBunchingModels/momentumContribution/morisonMomentum/morisonMomentum.C
@@ -45,16 +45,18 @@ morisonMomentum::morisonMomentum
     const Time& runTime
 )
 :
-    momentumContribution(runTime) //,
-    // Um_
-    // (
-    //     this->subDict(typeName + "Coeffs").lookup("Um")
-    // ),
-    // Tsoft_
-    // (
-    //     readScalar(this->subDict(typeName + "Coeffs").lookup("Tsoft"))
-    // )
-{}
+    momentumContribution(runTime),
+    Cdn_
+    (
+        readScalar(this->subDict(typeName + "Coeffs").lookup("Cdn"))
+    ),
+    Cdt_
+    (
+        readScalar(this->subDict(typeName + "Coeffs").lookup("Cdt"))
+    )
+{
+    Info<< "Found momentumContribution type: " << typeName << endl;
+}
 
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
@@ -62,19 +64,22 @@ morisonMomentum::morisonMomentum
 
 tmp<Field<scalarSquareMatrix>> morisonMomentum::diagCoeff
 (
-    const volVectorField& W, const volVectorField& Theta
+    const beamModel& bm,
+    const volVectorField& U
+    // const volVectorField& Accl
 )
 {
+    const volVectorField& W = bm.solutionW();
     // Take a reference to the mesh
     const fvMesh& mesh = W.mesh();
 
     // Prepare the result
     tmp<Field<scalarSquareMatrix>> tresult
      (
-         new Field<scalarSquareMatrix>
-         (
-             mesh.nCells(), scalarSquareMatrix(6, 6, 0.0)
-         )
+        new Field<scalarSquareMatrix>
+        (
+            mesh.nCells(), scalarSquareMatrix(6, 0.0)
+        )
      );
     // Field<scalarSquareMatrix>& result = tresult.ref();
 
@@ -84,15 +89,105 @@ tmp<Field<scalarSquareMatrix>> morisonMomentum::diagCoeff
 
 tmp<vectorField> morisonMomentum::linearMomentumSource
 (
-    const volVectorField& W, const volVectorField& Theta
+    const beamModel& bm,
+    const volVectorField& U
+    // const volVectorField& Accl
 )
 {
     // Take a reference to the mesh
-    const fvMesh& mesh = W.mesh();
+    const fvMesh& mesh = bm.solutionW().mesh();
+
+    // Flag to check the ddtSchemeName
+    const word ddtSchemeName
+    (
+        mesh.ddtSchemes().found("ddt(W)")
+        ?
+        (mesh.ddtSchemes().lookup("ddt(W)"))
+        :
+        (mesh.ddtSchemes().lookup("default"))
+    );
 
     // Prepare the result
     tmp<vectorField> tresult(new vectorField(mesh.nCells(), vector::zero));
-    // vectorField& result = tresult.ref();
+    vectorField& result = tresult.ref();
+
+    //- Drag forces due to Morison's Equation
+    if (ddtSchemeName != "steadyState")
+    {
+        // Create spline using current beam points and tangents data
+        HermiteSpline spline
+        (
+            bm.currentBeamPoints(),
+            bm.currentBeamTangents()
+        );
+
+        // Evaluate dRdS - tangents to beam centreline at beam CV cell-centres
+        const vectorField& dRdScell = spline.midPointDerivatives();
+
+        // Tangential component of velocity vector
+        vectorField Ut
+        (
+            (
+                (U.internalField() & dRdScell)
+                *dRdScell
+            )
+        );
+
+        vectorField UtHat (Ut/(mag(Ut) + SMALL));
+
+        // Normal component of velocity vector
+        vectorField Un
+        (
+            (
+                U.internalField()
+                - (
+                    (U.internalField() & dRdScell)
+                    *dRdScell
+                )
+            )
+        );
+
+        vectorField UnHat (Un/(mag(Un) + SMALL));
+
+        const scalar rho = bm.rho().value();
+        const scalar R = bm.R();
+        const volScalarField& L = bm.L();
+
+        // Scalar values of drag force (normal and tangential)
+        const scalarField Fdn(rho*Cdn_*R*L*(Un & Un));
+        const scalarField Fdt(rho*Cdt_*R*L*(Ut & Ut));
+
+    //     // Explicit drag forces included in the source vector
+    //     forAll(source, cellI)
+    //     {
+    //         source[cellI](0,0) += Fdn[cellI]*UnHat[cellI].component(0);
+    //         source[cellI](1,0) += Fdn[cellI]*UnHat[cellI].component(1);
+    //         source[cellI](2,0) += Fdn[cellI]*UnHat[cellI].component(2);
+
+    //         source[cellI](0,0) += Fdt[cellI]*UtHat[cellI].component(0);
+    //         source[cellI](1,0) += Fdt[cellI]*UtHat[cellI].component(1);
+    //         source[cellI](2,0) += Fdt[cellI]*UtHat[cellI].component(2);
+    //     }
+
+        forAll(result, cellI)
+        {
+            result[cellI] =
+                Fdn[cellI]*UnHat[cellI]
+              + Fdt[cellI]*UtHat[cellI];
+        }
+    }
+    else
+    {
+        FatalErrorInFunction
+        << "Momentum contribution due to Morison drag - type: "
+        << typeName
+        << " for time scheme " << ddtSchemeName
+        << " is not defined!!\n"
+        << "Set d2dt2Scheme and ddtScheme in system/fvSchemes as "
+        << "'Euler' or 'Newmark'"
+        << "to include drag force contributions"
+        << abort(FatalError);
+    }
 
     return tresult;
 }
@@ -100,9 +195,12 @@ tmp<vectorField> morisonMomentum::linearMomentumSource
 
 tmp<vectorField> morisonMomentum::angularMomentumSource
 (
-    const volVectorField& W, const volVectorField& Theta
+    const beamModel& bm,
+    const volVectorField& U
+    // const volVectorField& Accl
 )
 {
+    const volVectorField& W = bm.solutionW();
     // Take a reference to the mesh
     const fvMesh& mesh = W.mesh();
 
