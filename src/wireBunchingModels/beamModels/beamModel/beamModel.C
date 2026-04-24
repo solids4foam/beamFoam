6a476853df2b341082827e1c4575927a7b05fa5b
Amirhossein Taran <det.em.tw@gmail.com>
2026-04-24 10:53:49 +0200

    Fix rho check during beam model construction

--- src/wireBunchingModels/beamModels/beamModel/beamModel.C
+++ src/wireBunchingModels/beamModels/beamModel/beamModel.C
@@ -372,7 +372,10 @@ Foam::beamModel::beamModel
                 << " calculating the gravity body force"
                 << abort(FatalError);
         }
-        else if (mag(rho().value()) < SMALL)
+        else if
+        (
+            mag(dimensionedScalar("rho", beamProperties()).value()) < SMALL
+        )
         {
             WarningInFunction
                 << "rho field in constant/beamProperties = "
